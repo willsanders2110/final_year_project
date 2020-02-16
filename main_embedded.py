d1491fdab767a25915eab486a420575f45078a9d
willsanders2110 <willsanders2110@gmail.com>
2020-02-16 15:41:46 +0000

    Structure for the running code on the embedded device

---
+++ main_embedded.py
@@ -0,0 +1,17 @@
+# Will Sanders
+# 16/02/20
+# First Attempt at setting up the process
+# on the Raspberry Pi/Jetson Nano
+# =======================================
+
+# Calculate number of plants
+# This is going to be done by the user communicating with the device
+plant_list = []
+
+# Loop over number all plants in list
+for plant in plant_list:
+    print(plant)
+    # Get Image
+    # Get Soil Data
+
+# Store data
