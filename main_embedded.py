7fbe1f597936e93624da931332fa28df9fa5f44b
willsanders2110 <willsanders2110@gmail.com>
2020-02-16 16:26:55 +0000

    Update to main embedded structure

--- main_embedded.py
+++ main_embedded.py
@@ -8,10 +8,15 @@
 # This is going to be done by the user communicating with the device
 plant_list = []
 
+# It might be worth using the developed identification program as a
+# second step verification process
+
 # Loop over number all plants in list
 for plant in plant_list:
     print(plant)
     # Get Image
     # Get Soil Data
 
-# Store data
+# Store data until connected to the PC
+# Gotta figure out how its best to do this
+# Might use ROS or Image transferring package (ImageZMQ?)
