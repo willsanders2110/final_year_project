2b78d2aaee5e5f9d7940966e4e4c30ae1e08112e
willsanders2110 <willsanders2110@gmail.com>
2020-02-19 11:48:09 +0000

    Initial Read Soil Moisture Sensor test added

---
+++ read_sensor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import Rpi.GPIO as GPIO
+import time
+
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("Water Detected")
+    else:
+        print("Water Not Detected")
+
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    time.sleep(1)
