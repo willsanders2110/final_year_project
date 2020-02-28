cf0f46e4b27536077b6a0431a443dd7cb0887293
willsanders2110 <willsanders2110@gmail.com>
2020-02-28 16:13:24 +0000

    update socket trial

--- socket_server.py
+++ socket_server.py
@@ -9,11 +9,12 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 server_socket.listen(10)
 
-try:
-    server_socket.accept()
-    data = server_socket.recv(4096)
-    print(data.decode('utf-8'))
-    message = "Hi"
-    server_socket.send(message.encode('utf-8'))
-finally:
-    server_socket.close()
+while True:
+    connection = server_socket.accept()
+    try:
+        data = server_socket.recv(4096)
+        print(data.decode('utf-8'))
+        message = "Hi"
+        server_socket.send(message.encode('utf-8'))
+    finally:
+        server_socket.close()
