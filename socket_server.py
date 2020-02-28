e80d88ddef0cd6e9c69fbe63a49838e18922c8d8
willsanders2110 <willsanders2110@gmail.com>
2020-02-28 15:53:55 +0000

    Socket server readdition

---
+++ socket_server.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = '192.168.0.29'
+PORT = 8200
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen(10)
+
+
+while True:
+    server_socket.accept()
+    try:
+        data = server_socket.recv(4096)
+        print(data.decode('utf-8'))
+        message = "Hi"
+        server_socket.send(message.encode('utf-8'))
+    finally:
+        server_socket.close()
