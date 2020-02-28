6ca3ee0961a1a85e0d6b35a8ed74ccc6d2a15dd4
willsanders2110 <willsanders2110@gmail.com>
2020-02-28 16:39:43 +0000

    update socket trial

--- socket_server.py
+++ socket_server.py
@@ -9,15 +9,15 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 server_socket.listen(10)
 
-while True:
-    connection, address = server_socket.accept()
-    try:
-        print("got here")
-        data = connection.recv(4096)
-        print(data.decode('utf-8'))
-        message = "Hi"
-        connection.send(message.encode('utf-8'))
-    finally:
-        print("closing")
-        server_socket.close()
-        print("successfully closed")
+
+connection, address = server_socket.accept()
+try:
+    print("got here")
+    data = connection.recv(4096)
+    print(data.decode('utf-8'))
+    message = "Hi"
+    connection.send(message.encode('utf-8'))
+finally:
+    print("closing")
+    server_socket.close()
+    print("successfully closed")
