94efc88052f2e9fc572c8b824fa37b89e85ef541
willsanders2110 <willsanders2110@gmail.com>
2020-03-11 20:24:33 +0000

    run capture script directory update

--- run_capture.py
+++ run_capture.py
@@ -1,11 +1,8 @@
-from capture_image import Capture
 import schedule
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 import time
-
-capture = Capture()
-local_time = time.localtime()
-
-directory_name = 'images'
+import cv2
 
 
 def job():
@@ -14,10 +11,31 @@ def job():
     month = local_time.tm_mon
     image_name = '{}/{}_{}_{}'.format(directory_name, month, day, hr)
 
-    capture.image(image_name)
+    capture(image_name)
+
+
+def capture(image_name):
+    camera = PiCamera()
+    raw_capture = PiRGBArray(camera)
+
+    # allow the camera to warmup
+    time.sleep(0.1)
+
+    # Read time, and take image every hour or so
+    # Make sure that this can be updated by changing a variable!
+
+    # grab an image from the camera
+    camera.capture(raw_capture, format="bgr")
+    image = raw_capture.array
+
+    cv2.imwrite('{}.png'.format(image_name), image)
 
 
 schedule.every(1).minutes.do(job)
+
+local_time = time.localtime()
+
+directory_name = 'images'
 
 while True:
     schedule.run_pending()
