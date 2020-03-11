35de5cc045ed7ed9ddeb18e76853b29ae9ac733c
willsanders2110 <willsanders2110@gmail.com>
2020-03-11 16:17:01 +0000

    Updated capture image to be in class

--- capture_image.py
+++ capture_image.py
@@ -22,6 +22,6 @@ class Capture:
 
         cv2.imwrite('{}.png'.format(image_name), image)
 
-        cv2.imshow('image', image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # cv2.imshow('image', image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
