8502c565844f4d65383fcd8d8d716088423f4546
willsanders2110 <willsanders2110@gmail.com>
2020-03-11 18:50:56 +0000

    run capture script created

---
+++ run_capture.py
@@ -0,0 +1,24 @@
+from capture_image import Capture
+import schedule
+import time
+
+capture = Capture()
+local_time = time.localtime()
+
+directory_name = '../Desktop/images'
+
+
+def job():
+    hr = local_time.tm_hour
+    day = local_time.tm_mday
+    month = local_time.tm_mon
+    image_name = '{}/{}_{}_{}'.format(directory_name, month, day, hr)
+
+    capture.image(image_name)
+
+
+schedule.every(1).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
