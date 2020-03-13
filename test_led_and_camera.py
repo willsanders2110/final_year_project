dc1f4dea580c58016faa3cc99541dfbefaacda09
willsanders2110 <willsanders2110@gmail.com>
2020-03-13 12:24:27 +0000

    created test for capture image with led control

---
+++ test_led_and_camera.py
@@ -0,0 +1,36 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from led_setup import LED
+import time
+import cv2
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+led = LED()
+led.led_on()
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# Read time, and take image every hour or so
+# Make sure that this can be updated by changing a variable!
+
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+lt = time.localtime(time.time())
+time = "{}_{}_{}_{}_{}".format(lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min)
+
+# Check every time an image is taken whether it can be sent to the PC or not
+# If not, then store the data
+# If it can, check whether there is additional data to send, then send all
+
+# PC just needs to be pinged, the PC itself doesn't need to actively send that it's ready
+led.led_off()
+
+cv2.imshow('image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
