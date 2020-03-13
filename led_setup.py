ef09320e987bdb7de06e28ce631acfa3cd399fad
willsanders2110 <willsanders2110@gmail.com>
2020-03-13 12:19:36 +0000

    led class setup

---
+++ led_setup.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+
+class LED:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        self.LED_1 = 17
+        self.LED_2 = 27
+        GPIO.setup(self.LED_1, GPIO.OUT)
+        GPIO.setup(self.LED_2, GPIO.OUT)
+
+    def led_on(self):
+        print('LEDs on')
+        GPIO.output(self.LED_1, GPIO.HIGH)
+        GPIO.output(self.LED_2, GPIO.HIGH)
+
+    def led_off(self):
+        print('LEDs on')
+        GPIO.output(self.LED_1, GPIO.LOW)
+        GPIO.output(self.LED_2, GPIO.LOW)
