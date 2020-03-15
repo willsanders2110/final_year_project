a99f257d945405e877e26ed9b94aebf87e8d52a6
willsanders2110 <willsanders2110@gmail.com>
2020-03-15 21:05:02 +0000

    testing final run sequence

--- run_capture.py
+++ run_capture.py
@@ -5,12 +5,26 @@ from picamera.array import PiRGBArray
 import time
 import cv2
 from capture_soil_data import MoistureData
+from move_gantry import GantryControls
+import argparse
 
-ser = sl.Serial('/dev/ttyACM0', 9600)
-time.sleep(2)
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--number", type=int, help="number of plants in set")
+args = vars(ap.parse_args())
 
 
 def job():
+    capture_image()
+    get_moisture_data(1)
+
+    gantry.move_up_position(motor_connect)
+    capture_image()
+    get_moisture_data(2)
+    
+    gantry.move_home(motor_connect)
+
+
+def capture_image():
     local_time = time.localtime()
 
     minute = local_time.tm_min
@@ -19,18 +33,11 @@ def job():
     month = local_time.tm_mon
     image_name = '{}/{}_{}_{}_{}'.format(directory_name, month, day, hr, minute)
 
-    capture(image_name)
-
-
-def capture(image_name):
     camera = PiCamera()
     raw_capture = PiRGBArray(camera)
 
     # allow the camera to warmup
     time.sleep(0.1)
-
-    # Read time, and take image every hour or so
-    # Make sure that this can be updated by changing a variable!
 
     # grab an image from the camera
     camera.capture(raw_capture, format="bgr")
@@ -40,11 +47,34 @@ def capture(image_name):
 
     camera.close()
 
-    pos_1 = MoistureData.position_1_data()
-    print(pos_1)
+
+def get_moisture_data(val):
+    if val == 1:
+        moisture_1 = moisture_data.position_1_data(moisture_connect)
+        print(moisture_1)
+
+    elif val == 2:
+        moisture_2 = moisture_data.position_2_data(moisture_connect)
+        print(moisture_2)
+
+    elif val == 3:
+        moisture_3 = moisture_data.position_3_data(moisture_connect)
+        print(moisture_3)
+
+    elif val == 4:
+        moisture_4 = moisture_data.position_4_data(moisture_connect)
+        print(moisture_4)
 
 
-schedule.every().hour.do(job)
+moisture_connect = sl.Serial('/dev/ttyACM0', 9600)
+time.sleep(2)
+motor_connect = sl.Serial('/dev/ttyUSB0', 9600)
+time.sleep(2)
+
+moisture_data = MoistureData()
+gantry = GantryControls()
+
+schedule.every().minute.do(job)
 
 directory_name = 'images'
 
