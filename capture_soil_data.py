0a2064e4da8a06fd71e15a79bdd91ab092189f98
willsanders2110 <willsanders2110@gmail.com>
2020-03-15 21:14:54 +0000

    added encoding to serial write

--- capture_soil_data.py
+++ capture_soil_data.py
@@ -3,21 +3,21 @@
 
 class MoistureData:
     def position_1_data(self, ser):
-        ser.write('1')
+        ser.write('1'.encode())
         msg = ser.readline()
         return msg
 
     def position_2_data(self, ser):
-        ser.write('2')
+        ser.write('2'.encode())
         msg = ser.readline()
         return msg
 
     def position_3_data(self, ser):
-        ser.write('3')
+        ser.write('3'.encode())
         msg = ser.readline()
         return msg
 
     def position_4_data(self, ser):
-        ser.write('4')
+        ser.write('4'.encode())
         msg = ser.readline()
         return msg
--- move_gantry.py
+++ move_gantry.py
@@ -4,31 +4,31 @@ import time
 
 class GantryControls:
     def move_up_position(self, ser):
-        ser.write('1')
+        ser.write('1'.encode())
         time.sleep(2)
         msg = ser.readline()
         print("At position: {}".format(msg))
     
     def move_down_position(self, ser):
-        ser.write('2')
+        ser.write('2'.encode())
         time.sleep(2)
         msg = ser.readline()
         print("At position: {}".format(msg))
     
     def move_home(self, ser):
-        ser.write('3')
+        ser.write('3'.encode())
         time.sleep(6)
         msg = ser.readline()
         print("At position: {}".format(msg))
     
     def small_shift_right(self, ser):
-        ser.write('4')
+        ser.write('4'.encode())
         time.sleep(2)
         msg = ser.readline()
         print("At position: {}".format(msg))
     
     def small_shift_left(self, ser):
-        ser.write('5')
+        ser.write('5'.encode())
         time.sleep(2)
         msg = ser.readline()
         print("At position: {}".format(msg))
