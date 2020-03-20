b909bb21a4088e6b9ba76ce9bc3c2d8a41367a9d
willsanders2110 <willsanders2110@gmail.com>
2020-03-20 17:53:16 +0000

    Decoding move gantry message

--- move_gantry.py
+++ move_gantry.py
@@ -7,18 +7,21 @@ class GantryControls:
         ser.write('1'.encode())
         time.sleep(2)
         msg = ser.readline()
+        msg = msg.decode()
         print("At position: {}".format(msg))
     
     def move_down_position(self, ser):
         ser.write('2'.encode())
         time.sleep(2)
         msg = ser.readline()
+        msg = msg.decode()
         print("At position: {}".format(msg))
     
     def move_home(self, ser):
         ser.write('3'.encode())
         time.sleep(6)
         msg = ser.readline()
+        msg = msg.decode()
         print("At position: {}".format(msg))
     
     def small_shift_right(self, ser):
@@ -31,6 +34,7 @@ class GantryControls:
         ser.write('5'.encode())
         time.sleep(2)
         msg = ser.readline()
+        msg = msg.decode()
         print("At position: {}".format(msg))
 
 
