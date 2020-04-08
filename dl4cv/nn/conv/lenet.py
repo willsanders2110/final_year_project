47c0cf103054e3e940cbcec23366d1196d807735
willsanders2110 <willsanders2110@gmail.com>
2020-04-08 10:27:18 +0100

    Updated convolutional neural nets

--- dl4cv/nn/conv/lenet.py
+++ dl4cv/nn/conv/lenet.py
@@ -1,16 +1,16 @@
 # import the necessary packages
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras import backend as K
 
-class ShallowNet:
+class LeNet:
 	@staticmethod
 	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last"
+		# initialize the model
 		model = Sequential()
 		inputShape = (height, width, depth)
 
@@ -18,13 +18,23 @@ class ShallowNet:
 		if K.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 
-		# define the first (and only) CONV => RELU layer
-		model.add(Conv2D(32, (3, 3), padding="same",
+		# first set of CONV => RELU => POOL layers
+		model.add(Conv2D(20, (5, 5), padding="same",
 			input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+		# second set of CONV => RELU => POOL layers
+		model.add(Conv2D(50, (5, 5), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+		# first (and only) set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(500))
 		model.add(Activation("relu"))
 
 		# softmax classifier
-		model.add(Flatten())
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))
 
